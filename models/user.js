1255f809d4e198db883fd3bfc3a509c218a2a24a
EduardMLT <eddku@i.ua>
2023-12-16 20:10:04 +0200

    initial_03

--- models/user.js
+++ models/user.js
@@ -22,8 +22,8 @@ const userSchema = new Schema(
     },
     subscription: {
       type: String,
-      enum: ["starter", "pro", "business"],
-      default: "starter",
+      enum: ["weight_loss", "muscle_gain", "weight_maintenance"],
+      default: "weight_loss",
     },
     token: {
       type: String,
@@ -57,56 +57,3 @@ userSchema.post("save", handleMongooseError);
 const User = model("user", userSchema);
 
 module.exports = { User, emailRegexp };
-
-
-
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: [true, "Name for user"],
-//     },
-//     password: {
-//       type: String,
-//       required: [true, "Set password for user"],
-//     },
-//     email: {
-//       type: String,
-//       required: [true, "Email is required"],
-//       unique: true,
-//         }, 
-//     subscription: {
-//       type: String,
-//       enum: ["starter", "pro", "business"],
-//       default: "starter",
-//     },
-//     token: {
-//       type: String,
-//       default: null,
-//     },
-//     avatar: {
-//       type: String,
-//       default: null,
-//     },
-//     verify: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     verifyToken: {
-//       type: String,
-//       default: null,
-//     },
-//     confirmationOfVerification: {
-//       type: Number,
-//       default: 0,
-//     },
-//   },
-//   {
-//     versionKey: false,
-//     timestamps: true,
-//   }
-// );
-
-// module.exports = mongoose.model("User", userSchema);
