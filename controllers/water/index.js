94673e8561680657a5faf56dd0e1134b7d0ac5a0
AntonTy35 <antony.tislenko@gmail.com>
2023-12-21 12:47:10 +0200

    fix

--- controllers/water/index.js
+++ controllers/water/index.js
@@ -1,8 +1,8 @@
 const { ctrlWrapper } = require("../../helpers");
-const deleteWater = require("./waterDelete");
+// const deleteWater = require("./waterDelete");
 const waterIntake = require("./waterIntake");
 
 module.exports = {
   waterIntake: ctrlWrapper(waterIntake),
-  deleteWater: ctrlWrapper(deleteWater),
+  // deleteWater: ctrlWrapper(deleteWater),
 };
