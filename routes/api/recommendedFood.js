d1eade664bdcc6f9b24d40c0baacce03856717d4
AntonTy35 <antony.tislenko@gmail.com>
2023-12-23 17:02:59 +0200

    auth

--- routes/api/recommendedFood.js
+++ routes/api/recommendedFood.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-// const { authenticate } = require("../../middlewares");
+const { authenticate } = require("../../middlewares");
 
 const ctrl = require("../../controllers/recommendedFood");
 
 const router = express.Router();
 
-router.get("/recommended-food", ctrl.getRecommendedFood);
+router.get("/recommended-food", authenticate, ctrl.getRecommendedFood);
 
 module.exports = router;
