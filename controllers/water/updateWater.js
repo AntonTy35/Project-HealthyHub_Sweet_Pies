30fee699f5a4789a5f49dc8767eb530fe18b6d2f
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-12-27 22:06:03 +0200

    water-intake_updated

--- controllers/water/updateWater.js
+++ controllers/water/updateWater.js
@@ -1,45 +1,58 @@
 const { WaterModel } = require("../../models/water");
 const formattedDate = require("../../utils/formattedDate");
 
+// Функція для створення нового запису води
+const createNewWaterEntry = async (owner, body) => {
+  const currentDate = formattedDate();
+  const newWater = await WaterModel.create({
+    ...body,
+    date: currentDate,
+    owner,
+  });
+
+  return {
+    date: currentDate,
+    water: newWater.water,
+  };
+};
+
+// Функція для оновлення існуючого запису води
+const updateExistingWaterEntry = async (existingWater, body) => {
+  existingWater.water += Number(body.water);
+  await existingWater.save();
+
+  return {
+    date: existingWater.date,
+    water: existingWater.water,
+  };
+};
+
 const updateWater = async (req, res, next) => {
   try {
     const { _id: owner } = req.user;
     const body = req.body;
     const currentDate = formattedDate();
 
+    // Пошук існуючого запису води за вказаною датою
     const existingWater = await WaterModel.findOne({
       owner,
       date: currentDate,
     });
 
+    let responseData;
+
     if (!existingWater) {
-      const newWater = await WaterModel.create({
-        ...body,
-        date: currentDate,
-        owner,
-      });
-
-      res.status(201).json({
-        status: "success",
-        code: 201,
-        data: {
-          date: currentDate,
-          water: newWater.water,
-        },
-      });
+      // Якщо запис не існує, створити новий
+      responseData = await createNewWaterEntry(owner, body);
+      res
+        .status(201)
+        .json({ status: "success", code: 201, data: responseData });
     } else {
-      // Додаємо кількість води до існуючого запису
-      existingWater.water = Number(existingWater.water) + Number(body.water);
-      await existingWater.save();
-
-      res.status(200).json({
-        status: "success",
-        code: 200,
-        data: {
-          date: currentDate,
-          water: existingWater.water,
-        },
-      });
+      // Якщо запис існує, оновити його
+      responseData = await updateExistingWaterEntry(existingWater, body);
+      res
+        .status(200)
+        .json({ status: "success", code: 200, data: responseData });
     }
   } catch (error) {
     console.error(error);
